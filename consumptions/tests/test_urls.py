f2c928f3f0e3caec3389b64f33ddcb22b0fe09ca
konykon <tsagarikon@hotmail.com>
2021-01-14 23:42:00 +0100

    added more tests

--- consumptions/tests/test_urls.py
+++ consumptions/tests/test_urls.py
@@ -1,63 +1,94 @@
 from django.test import SimpleTestCase, TestCase
 from django.urls import path, reverse, resolve
+from rest_framework import status
+from rest_framework.test import APIClient
+from consumptions.models import Consumption, Product, Product_Category
+from consumptions.serializers import ConsumptionSerializer
 from consumptions.views import *
 
-class TestUrls(SimpleTestCase):
 
-    def test_index_url_is_resolved(self):
-        url = reverse('index')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, index)
+CONSUMPTIONS_URL = reverse('consumptions:consumption-list')
 
-    def test_api_list_url_is_resolved(self):
-        url = reverse('api')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionApi)
 
-    def test_api_create_url_is_resolved(self):
-        url = reverse('api_create')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionCreateApi)
+def sample_product_category(**params):
+    """Create and return a sample product category"""
+    defaults = {
+        'code': 'AA',
+        'name': 'testname'
+    }
+    defaults.update(params)
 
-    def test_list_url_is_resolved(self):
-        url = reverse('consumptions')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, filter_by_product_category)
+    return Product_Category.objects.create(**defaults)
 
-    def test_create_url_is_resolved(self):
-        url = reverse('consumption_create')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionCreate)
+def sample_product(**params):
+    """Create and return a sample product"""
+    defaults = {
+        'code': 'AA001',
+        'name': 'testproductname',
+        'category': sample_product_category()
+    }
+    defaults.update(params)
 
-    def test_upload_csv_url_is_resolved(self):
-        url = reverse('upload_product_categories_csv')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, upload_product_categories_csv)
+    return Product.objects.create(**defaults)
 
-    
-class TestUrlsWithArgs(TestCase):
+def sample_consumption(**params):
+    """Create and return a sample consumption"""
+    # import pdb; pdb.set_trace()
+    defaults = {
+        'product': sample_product(),
+        'quantity': 10
+    }
+    defaults.update(params)
+
+    return Consumption.objects.create(**defaults)
+
+class TestUrls(TestCase):
 
     def setUp(self):
-        self.prod_cat = Product_Category.objects.create(code='LL', name='landline')
-        self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
-        self.cons = Consumption.objects.create(product=self.prod, quantity='10')
+        self.client = APIClient()
+        # self.prod_cat = Product_Category.objects.create(code='LL', name='landline')
+        # self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
+        # self.cons = Consumption.objects.create(product=self.prod, quantity='10')
 
-    def test_update_url_is_resolved(self):
-        url = reverse('api_update', kwargs={'pk': 1})
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionUpdateApi)
+    def test_retrieve_consumptions(self):
+        """Test retrieving a list of consumptions"""
+        sample_consumption()
 
-    def test_update_api_url_is_resolved(self):
-        url = reverse('api_delete', kwargs={'pk': 1})
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionDeleteApi)
+        res = self.client.get(CONSUMPTIONS_URL)
 
-    def test_delete_api_url_is_resolved(self):
-        url = reverse('consumption_update', kwargs={'pk': 1})
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionUpdate)
+        consumptions = Consumption.objects.all().order_by('-id')
+        serializer = ConsumptionSerializer(consumptions, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-    def test_delete_url_is_resolved(self):
-        url = reverse('consumption_delete', kwargs={'pk': 1})
-        print(resolve(url))
-        self.assertEquals(resolve(url).func.view_class, ConsumptionDelete)
+    def test_index_url_is_resolved(self):
+        url = reverse('consumptions:index')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEquals(resolve(url).func.view_class, Index)
+
+    def test_create_url_is_resolved(self):
+        url = reverse('consumptions:consumption_create')
+        self.assertEquals(resolve(url).func.view_class, ConsumptionCreate)
+
+    def test_upload_product_csv_url_is_resolved(self):
+        url = reverse('consumptions:upload_product_csv')
+        self.assertEquals(resolve(url).func, upload_product_csv)
+
+    
+# class TestUrlsWithArgs(TestCase):
+
+#     def setUp(self):
+#         self.prod_cat = Product_Category.objects.create(code='LL', name='landline')
+#         self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
+#         self.cons = Consumption.objects.create(product=self.prod, quantity='10')
+
+#     def test_update_url_is_resolved(self):
+#         url = reverse('consumption_update', kwargs={'pk': 1})
+#         print(resolve(url))
+#         self.assertEquals(resolve(url).func.view_class, ConsumptionUpdate)
+
+#     def test_delete_url_is_resolved(self):
+#         url = reverse('consumption_delete', kwargs={'pk': 1})
+#         print(resolve(url))
+#         self.assertEquals(resolve(url).func.view_class, ConsumptionDelete)
--- consumptions/tests/test_views.py
+++ consumptions/tests/test_views.py
@@ -1,44 +1,46 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 from consumptions.models import *
 import json
+
+
+CONSUMPTIONS_URL = reverse('consumptions:consumption-list')
+
 
 class TestViews(TestCase):
 
     def setUp(self):
-        self.client = Client()
+        self.client = APIClient()
         self.prod_cat = Product_Category.objects.create(code='LL', name='landline')
         self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
         self.cons = Consumption.objects.create(product=self.prod, quantity=10)
 
-
-    def test_index_get(self):
-        response = self.client.get(reverse('index'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
-
-    def test_consumption_table_get(self):
-        response = self.client.get(reverse('consumptions'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'consumptions.html')
+    def test_consumptions_list_get(self):
+        """Test list of consumptions render and view"""
+        res = self.client.get(reverse('consumptions:consumption_list'))
+        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(res, 'consumption_list.html')
 
     def test_consumption_create_get(self):
-        response = self.client.get(reverse('consumption_create'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'consumption_form.html')
+        """Test consumptions create render and view"""
+        res = self.client.get(reverse('consumptions:consumption_create'))
+        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(res, 'consumption_form.html')
 
-    def test_consumption_create_post(self):
-        response = self.client.post(reverse('consumption_create'), {
-            'product': 'LL001', 
-            'quantity': 10
-        })
-        self.assertEquals(response.status_code, 302)
-        self.assertEquals(self.prod.code, 'LL001')
+#     def test_consumption_create_post(self):
+#         response = self.client.post(reverse('consumption_create'), {
+#             'product': 'LL001', 
+#             'quantity': 10
+#         })
+#         self.assertEquals(response.status_code, 302)
+#         self.assertEquals(self.prod.code, 'LL001')
 
 
-    def test_consumption_delete_delete(self):
-        response = self.client.delete(reverse('consumption_delete', kwargs={'pk': self.cons.pk}))
-        self.assertEquals(response.status_code, 302)
+#     def test_consumption_delete_delete(self):
+#         response = self.client.delete(reverse('consumption_delete', kwargs={'pk': self.cons.pk}))
+#         self.assertEquals(response.status_code, 302)
 
 
 
