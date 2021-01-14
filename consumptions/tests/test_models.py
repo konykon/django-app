665772b6822921ef6140a06c8eb5a5f2eb6a4d94
konykon <tsagarikon@hotmail.com>
2021-01-14 23:37:36 +0100

    refactored functional views as class views
    This commit unfortunately has much more changes than those its name reveals.
    The import csv method is added in views, as well as factory boy is used in test_models!

---
+++ consumptions/tests/test_models.py
@@ -0,0 +1,66 @@
+from django.test import TestCase
+
+from consumptions.models import Consumption, Product_Category, Product
+
+import factory
+from datetime import datetime
+
+
+class Product_Category_Factory(factory.Factory):
+    class Meta:
+        model = Product_Category
+
+    code = 'LL'
+    name = 'Landline'
+
+
+class Product_Factory(factory.Factory):
+    class Meta:
+        model = Product
+
+    code = 'LL001'
+    name = 'ADSL'
+    category = factory.SubFactory(Product_Category_Factory)
+
+
+class Consumption_Factory(factory.Factory):
+    class Meta:
+        model = Consumption
+
+    timestamp = factory.LazyFunction(datetime.now)
+    product = factory.SubFactory(Product_Factory)
+    quantity = 10
+
+class Product_Category_ModelTest(TestCase):
+    def setUp(self):
+        self.prod_cat = Product_Category.objects.create(code='LL', name='landline')
+        self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
+        self.cons = Consumption.objects.create(product=self.prod, quantity=10)
+
+    def test_product_category_string(self):
+        """Test product category name and code are strings"""
+        self.assertEqual(Product_Category_Factory().code, 'LL')
+        self.assertEqual(Product_Category_Factory().name, 'Landline')
+
+    def test_product_category_is_instance(self):
+        """Tests product category is created"""
+        self.assertTrue(isinstance(Product_Category_Factory(), Product_Category))
+
+
+class Product_ModelTest(TestCase):
+
+    def test_product_string(self):
+        """Test product name and code are strings"""
+        self.assertEqual(Product_Factory().code, 'LL001')
+        self.assertEqual(Product_Factory().name, 'ADSL')
+
+    def test_product_is_instance(self):
+        """Tests product is created"""
+        self.assertTrue(isinstance(Product_Factory(), Product))
+
+
+class Consumption_ModelTest(TestCase):
+
+    def test_product_is_instance(self):
+        """Test consumption is created"""
+        self.assertTrue(isinstance(Consumption_Factory(), Consumption))
