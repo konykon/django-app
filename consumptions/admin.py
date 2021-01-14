78cea4918f2f8964653b7a41cae35c585784be48
konykon <tsagarikon@hotmail.com>
2021-01-14 23:38:27 +0100

    added  admin import csv

--- consumptions/admin.py
+++ consumptions/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from .models import Consumption, Product_Category, Product
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class ProductCategoryResource(resources.ModelResource):
+    class Meta:
+        model = Product_Category
+
+
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+
+
+class Product_CategoryAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    resource_class = ProductCategoryResource
+
+
+class ProductAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    resource_class = ProductResource
+
 
 # Register your models here.
-admin.site.register(Product_Category)
-admin.site.register(Product)
+admin.site.register(Product_Category, Product_CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Consumption)
-
