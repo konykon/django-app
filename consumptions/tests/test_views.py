07ffab44ebf35e84a36a3fc94a5e09c785812d58
konykon <tsagarikon@hotmail.com>
2021-01-27 09:24:04 +0100

    pass tests after changing urls & refactoring views

--- consumptions/tests/test_views.py
+++ consumptions/tests/test_views.py
@@ -11,25 +11,24 @@ class TestViews(TestCase):
         self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
         self.cons = Consumption.objects.create(product=self.prod, quantity=10)
 
-
     def test_index_get(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('consumptions:index'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
     def test_consumption_table_get(self):
-        response = self.client.get(reverse('consumptions'))
+        response = self.client.get(reverse('consumptions:consumption_list'))
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'consumptions.html')
+        self.assertTemplateUsed(response, 'consumption_list.html')
 
     def test_consumption_create_get(self):
-        response = self.client.get(reverse('consumption_create'))
+        response = self.client.get(reverse('consumptions:consumption_create'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'consumption_form.html')
 
 
     def test_consumption_delete_delete(self):
-        response = self.client.delete(reverse('consumption_delete', kwargs={'pk': self.cons.pk}))
+        response = self.client.delete(reverse('consumptions:consumption_delete', kwargs={'pk': self.cons.pk}))
         self.assertEquals(response.status_code, 302)
 
 
