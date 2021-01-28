051bbda9e6f65123a6ff505b226c63aaae9caa57
konykon <tsagarikon@hotmail.com>
2021-01-28 16:50:04 +0100

    use factory boy fake data to test models

--- consumptions/tests/test_models.py
+++ consumptions/tests/test_models.py
@@ -1,31 +1,35 @@
 from django.test import TestCase
 
 from consumptions.models import Consumption, Product_Category, Product
+from .factories import (Consumption_Factory, Product_Category_Factory,
+                        Product_Factory)
+
 
 class Product_CategoryModelTest(TestCase):
-    def setUp(self):
-        self.prod_cat = Product_Category.objects.create(code='LL', name='landline')
-        self.prod = Product.objects.create(code='LL001', name='ADSL', category=self.prod_cat)
-        self.cons = Consumption.objects.create(product=self.prod, quantity=10)
+    def test_product_category_strings(self):
+        """Test product category name and code are strings"""
+        self.assertEqual(Product_Category_Factory().name, 'Landline')
+        self.assertEqual(Product_Category_Factory().code, 'LL')
 
-    def test_product_category_name_string_name(self):
-        self.assertEqual(str(self.prod_cat.name), 'landline')
-
-    def test_product_category_name_string_code(self):
-        self.assertEqual(str(self.prod_cat), 'LL')
-
-    def test_product_category_name_string_name(self):
-        self.assertEqual(str(self.prod_cat.name), 'landline')
-  
-    def test_product_string(self):
+    def test_product_strings(self):
         """Test product name and code are strings"""
-        self.assertEqual(self.prod.code, 'LL001')
-        self.assertEqual(self.prod.name, 'ADSL')
+        self.assertEqual(Product_Factory().code, 'LL001')
+        self.assertEqual(Product_Factory().name, 'ADSL')
+
+    def test_consumption_quantity_is_int(self):
+        """Test quantity is integer"""
+        self.assertEqual(Consumption_Factory().quantity, 10)
+
+    def test_product_category_is_instance(self):
+        """Test product category is created"""
+        self.assertTrue(isinstance(
+            Product_Category_Factory(), Product_Category)
+        )
 
     def test_product_is_instance(self):
-        """Tests product is created"""
-        self.assertTrue(isinstance(self.prod, Product))
+        """Test product is created"""
+        self.assertTrue(isinstance(Product_Factory(), Product))
 
-    def test_product_is_instance(self):
+    def test_consumption_is_instance(self):
         """Test consumption is created"""
-        self.assertTrue(isinstance(self.cons, Consumption))
+        self.assertTrue(isinstance(Consumption_Factory(), Consumption))
