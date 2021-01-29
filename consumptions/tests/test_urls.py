add302ba3276533a31d8dd16f8c9301045f6b5fe
konykon <tsagarikon@hotmail.com>
2021-01-29 11:47:03 +0100

    replace deprecated assertEquals()

--- consumptions/tests/test_urls.py
+++ consumptions/tests/test_urls.py
@@ -9,26 +9,26 @@ class TestUrls(SimpleTestCase):
     def test_index_url_is_resolved(self):
         """Test index url is resolved"""
         url = reverse('consumptions:index')
-        self.assertEquals(resolve(url).func.view_class, Index)
+        self.assertEqual(resolve(url).func.view_class, Index)
 
     def test_list_url_is_resolved(self):
         """Test consumptions list url is resolved"""
         url = reverse('consumptions:consumption_list')
-        self.assertEquals(resolve(url).func.view_class, ConsumptionList)
+        self.assertEqual(resolve(url).func.view_class, ConsumptionList)
 
     def test_create_url_is_resolved(self):
         """Test create consumption url is resolved"""
         url = reverse('consumptions:consumption_create')
-        self.assertEquals(resolve(url).func.view_class, ConsumptionCreate)
+        self.assertEqual(resolve(url).func.view_class, ConsumptionCreate)
 
     def test_upload_csv_url_is_resolved(self):
         """Test upload csv url is resolved"""
         url = reverse('consumptions:upload_product_csv')
-        self.assertEquals(resolve(url).func, upload_product_csv)
+        self.assertEqual(resolve(url).func, upload_product_csv)
 
 
 class TestUrlsWithArgs(TestCase):
     def test_delete_url_is_resolved(self):
         """Test delete consumption url is resolved"""
         url = reverse('consumptions:consumption_delete', kwargs={'pk': 1})
-        self.assertEquals(resolve(url).func.view_class, ConsumptionDelete)
+        self.assertEqual(resolve(url).func.view_class, ConsumptionDelete)
--- consumptions/tests/test_views.py
+++ consumptions/tests/test_views.py
@@ -13,17 +13,17 @@ class TestViews(TestCase):
     def test_index_get(self):
         """Test retrieving the home page"""
         res = self.client.get(reverse('consumptions:index'))
-        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertTemplateUsed(res, 'index.html')
 
     def test_consumption_table_get(self):
         """Test retrieving a consumptions list"""
         res = self.client.get(reverse('consumptions:consumption_list'))
-        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertTemplateUsed(res, 'consumption_list.html')
 
     def test_consumption_create_get(self):
         """Test retrieving the create view"""
         res = self.client.get(reverse('consumptions:consumption_create'))
-        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertTemplateUsed(res, 'consumption_form.html')
